ad9377a5e710b43b75fd8efcecbec6ac5afdd880
flaviodsilverio <flaviosilverio@mbp-de-flavio.lan>
2016-09-13 18:25:51 +0100

    Initial Commit

---
+++ SimpleCoredataExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SimpleCoredataExample
+//
+//  Created by Flávio Silvério on 13/09/16.
+//  Copyright © 2016 Flávio Silvério. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
